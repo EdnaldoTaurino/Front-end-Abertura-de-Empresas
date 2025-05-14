98f77203bc612fa1510d26e9fe2712452fb830ed
EdnaldoTaurino <ednaldo.taurino5@gmail.com>
2025-05-13 23:18:52 -0300

    Remove configurações de produção do ambiente e atualiza URLs da API para o ambiente de desenvolvimento

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBase: 'https://abertura-de-empresas.vercel.app',
-  apiPort: 4443,
-  apiUrl: 'https://abertura-de-empresas.vercel.app/empresas',
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,14 @@
 export const environment = {
   production: false,
   apiPort: 8080,
-  apiBase: 'http://localhost',
-  apiUrl: 'http://localhost:8080/empresas',
+  apiBase: 'https://testebackapi.vercel.app/',
+  apiUrl: 'https://testebackapi.vercel.app/empresas',
 };
+
+// local quando precisar usar
+// export const environment = {
+//   production: false,
+//   apiPort: 3000,
+//   apiBase: 'http://localhost',
+//   apiUrl: 'http://localhost:3000/empresas',
+// };
